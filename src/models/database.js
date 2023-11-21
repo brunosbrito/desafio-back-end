cbf97254bf48426143829792b8dc39ee1e33d9bf
brunosbrito <brunobrito728@gmail.com>
2023-11-21 18:10:08 -0300

    configuracao heroku

--- src/models/database.js
+++ src/models/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
+  host: 'qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'desafio',
+  user: 'flaa13oibyxwub0k',
+  password: 'j5mq67pqtwfjqwr1',
+  database: 'v7o8zmq9di0rbvzc',
   
 });
 
