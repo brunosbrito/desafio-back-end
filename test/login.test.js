3e6470855b03ad8d86d5050ecb47e78daa034d68
brunosbrito <brunobrito728@gmail.com>
2023-11-21 00:40:59 -0300

    test: criado os teste do endpoint de login

---
+++ test/login.test.js
@@ -0,0 +1,34 @@
+const supertest = require('supertest');
+const app = require('../src/app');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('POST /login', () => {
+  it('Deve fazer login com sucesso', async () => {
+    const response = await supertest(app)
+      .post('/login')
+      .send({
+        email: 'bruno@teste.com',
+        senha: '12345678'
+      });
+
+    expect(response.status).to.equal(201);
+    expect(response.body).to.have.property('id');
+    expect(response.body).to.have.property('data_criacao');
+    expect(response.body).to.have.property('data_atualizacao');
+    expect(response.body).to.have.property('ultimo_login');
+    expect(response.body).to.have.property('token');
+  });
+
+  it('Deve falhar ao fazer login com Usuário e/ou senha inválidos', async () => {
+    const response = await supertest(app)
+      .post('/login')
+      .send({
+        email: 'novo_usuario@example.com',
+        senha: 'senha_incorreta'
+      });
+
+      expect(response.status).to.equal(401);
+    expect(response.body).to.have.property('mensagem', 'Usuário e/ou senha inválidos');
+  });
+});
